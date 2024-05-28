69ed509ea05c95009c2a8399f879cfcfd62592ae
Yunus Bayraktar <165094867+yunusbayraktar1@users.noreply.github.com>
2024-05-28 23:23:15 +0300

    I am still trying to learn git commands
    I had problems uploading some files, but since it is a simple application, I think it is not a big problem.

---
+++ App.js
@@ -0,0 +1,21 @@
+import {useState} from 'react';
+import './App.css';
+
+function App() {
+  const[hex,setHex] = useState("#ffffff");
+
+  function randomHex () {
+    const randomhex = "#"+(Math.floor(Math.random() * 16777721).toString(16));
+    setHex(randomhex);
+  }
+  return (
+    <div className="App" style={{backgroundColor:`${hex}`,transition:".3s all"}}>
+      <h1>{hex}</h1>
+      <button onClick={randomHex}>click for new background</button>
+      <button onClick={()=>navigator.clipboard.writeText(hex)}>copy the hex value</button>
+     
+    </div>
+  );
+}
+
+export default App;
